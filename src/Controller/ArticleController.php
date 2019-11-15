409a7d5ae7bfe53fd74f285d5a97582c0929fdd6
Issam <issam_derrar@yahoo.fr>
2019-11-15 12:22:38 +0100

    even more progress

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,28 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ArticleController
 {
 
+    /**
+     * @Route("/")
+     */
+    public function homePage()
+    {
+        return new Response('OMG MY FIRST PAGE ALREADY');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show one space article: %s',
+            $slug
+            ));
+    }
 }
